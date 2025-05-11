0403ff11815a2c3dc88f0647f22c00069ff3e699
aohajin <maohaijian@xiimoon.com>
2025-05-11 13:50:55 +0800

    test version

--- AwakeningUpdater.lua
+++ AwakeningUpdater.lua
@@ -1,1 +1,35 @@
-print("awakening loaded!")
+local _, AUP = ...
+
+--local LDB = LibStub("LibDataBroker-1.1")
+--local LDBIcon = LibStub("LibDBIcon-1.0")
+local eventFrame = CreateFrame("Frame")
+
+eventFrame:RegisterEvent("ADDON_LOADED")
+
+eventFrame:SetScript(
+    "OnEvent",
+    function(_, event, ...)
+        if event == "ADDON_LOADED" then
+            local addOnName = ...
+
+            if addOnName == "AwakeningUpdater" then
+                if not AwakeningUpdaterSaved then AwakeningUpdaterSaved = {} end
+                if not AwakeningUpdaterSaved.minimap then AwakeningUpdaterSaved.minimap = {} end
+                if not AwakeningUpdaterSaved.settings then AwakeningUpdaterSaved.settings = {} end
+                if not AwakeningUpdaterSaved.settings.frames then AwakeningUpdaterSaved.settings.frames = {} end
+
+
+                AUP:InitializeWeakAurasImporter()
+                AUP:InitializeInterface()
+                AUP:InitializeAuraUpdater()
+                AUP:InitializeAuraChecker()
+            end
+        end
+    end
+)
+
+SLASH_AwakeningUPDATER1, SLASH_AwakeningUPDATER2, SLASH_AwakeningUPDATER3 = "/au", "/Awakeningupdate",
+    "/Awakeningupdater"
+function SlashCmdList.AwakeningUPDATER()
+    AUP.window:SetShown(not AUP.window:IsShown())
+end
